5d9081fb9ac94719224225a9588994ea9ccab6ab
23eliu <23eliu@athenian.org>
2020-10-28 16:29:17 +0000

    finished classwork

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,69 @@
-public class Student {
-    
+public class Student extends Person {
+    private int classYear;
+
+    public Student(String a, String b, int c) {
+        super(a, b);
+        this.classYear = c;
+    }
+
+    public Student() {
+        super("Jerry", "Seinfeld");
+        this.classYear = 2024;
+    }
+
+    public Student(int c) {
+        super(); // if you don't call super(), it'll automatically call super() with no arguments
+        this.classYear = c;
+    }
+
+    public String toString() {
+        return this.getFirstName() + " " + this.getLastName() + "," + this.classYear;
+    }
 }
+/*
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int classYear;
+
+    public Student(String a, String b, int c) {
+        this.firstName = a;
+        this.lastName = b;
+        this.classYear = c;
+    }
+
+    public Student() {
+        this.firstName = "Jerry";
+        this.lastName = "Seinfeld";
+        this.classYear = 2024;
+    }
+
+    public void doSomething() {
+        System.out.println("I'm doing my homework");
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getClassYear() {
+        return this.classYear;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.trim().length() != 0) {
+            this.firstName = firstName;
+        }
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.trim().length() != 0) {
+            this.lastName = lastName;
+        }
+    }
+}
+*/
--- src/main/java/StudentClient.java
+++ src/main/java/StudentClient.java
@@ -1,5 +1,14 @@
 public class StudentClient {
     public static void main(String[] args) {
-        
+        Student s1 = new Student("Bob", "Dylan", 2021);
+        System.out.println(s1);
+
+        Student s2 = new Student();
+        System.out.println(s2);
+
+        s1.doSomething();
+
+        Student s3 = new Student(2024);
+        System.out.println(s3);
     }
 }
